f16434f0821703bd6a2496a1beb63e458e2611a8
Hussein Mohammadi <hussain.muhammadi.1234@gmail.com>
2025-01-28 13:34:06 +0100

    Task 5 down

---
+++ TDDTest1/Task/Task 5/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTest1.Task.Task_5
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        public decimal Balance => _balance; // Läsbart saldo
+
+        public BankAccount(decimal initialBalance = 0)
+        {
+            if (initialBalance < 0)
+            {
+                throw new ArgumentException("Initial balance cannot be negative.");
+            }
+            _balance = initialBalance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be greater than zero.");
+            }
+
+            _balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Withdrawal amount must be greater than zero.");
+            }
+
+            if (amount > _balance)
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+
+            _balance -= amount;
+        }
+    }
+}
--- TDDTest1/obj/Debug/net8.0/TDDTest1.AssemblyInfo.cs
+++ TDDTest1/obj/Debug/net8.0/TDDTest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDDTest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c308bcf8d9d549cbdfe28eab0de8e6927567f419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda9eaa634425072fe6a999f361ccc9c49738ce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDDTest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDDTest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
